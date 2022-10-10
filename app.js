c8d0e703f5b22b3fc3b12fec3342bf12a4e452e1
Kyle <kmthayer@uw.edu>
2022-10-10 10:19:53 -0700

    remove the index.js route which is unused

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 
-import indexRouter from './routes/index.js';
 import usersRouter from './routes/users.js';
 
 import { fileURLToPath } from 'url';
@@ -20,7 +19,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 export default app;
